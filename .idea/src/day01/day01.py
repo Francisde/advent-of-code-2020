a23f2cd3e736224a5a08bcba2245b2c92e621e51
Martin Mehlhose <mehlhose@rvs.informatik.uni-leipzig.de>
2024-11-18 09:37:30 +0100

    solve day 01 part 01

--- .idea/src/day01/day01.py
+++ .idea/src/day01/day01.py
@@ -1,15 +1,26 @@
+def solve_part_one(input_list):
+    result = 0
+    for int1 in expense_list:
+        for int2 in expense_list:
+            if int1 != int2:
+                if int1 + int2 == 2020:
+                    return int1 * int2
+    return result
 
-file1 = open('test.txt', 'r')
+file1 = open('puzzle.txt', 'r')
 Lines = file1.readlines()
 
 count = 0
 
+expense_list = []
+
 for line in Lines:
     input_line= line.strip()
     print("Line {}: {}".format(count, input_line))
+    expense_list.append(int(input_line))
     count += 1
 
 
-print("TASK 1 - ")
+print("TASK 1 - sol: {}".format(solve_part_one(expense_list)))
 
 print("TASK 2 - ")
